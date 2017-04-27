7a1a185c089bd98dcc7d06a602a5a3d5df18235a
Chris Kolosiwsky <ckolos@mozilla.com>
2017-04-27 11:00:37 -0500

    entrypoint changes
     - conditionally set vars
     - use of a case statement allows us to get more functionality from 1
    container. In this case (hah), we're able to set up storage, start the
    app, create a volume container, start a admin shell, and do a db
    dump/backup with the same container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,36 +9,70 @@
 # Configure Phabricator on startup from environment variables.
 
 set -ex
-
 cd phabricator
-
-# Set the local repository and make sure it is readable by the web user.
-./bin/config set repository.default-local-path "${REPOSITORY_LOCAL_PATH}"
-chown -R www-data:www-data $REPOSITORY_LOCAL_PATH
-
-./bin/config set mysql.host ${MYSQL_HOST}
-./bin/config set mysql.port ${MYSQL_PORT}
-./bin/config set mysql.user ${MYSQL_USER}
+test -n "${MYSQL_HOST}" \
+  && ./bin/config set mysql.host ${MYSQL_HOST}
+test -n "${MYSQL_PORT}" \
+  && ./bin/config set mysql.port ${MYSQL_PORT}
+test -n "${MYSQL_USER}" \
+  && ./bin/config set mysql.user ${MYSQL_USER}
 set +x
-./bin/config set mysql.pass ${MYSQL_PASS}
+test -n "${MYSQL_USER}" \
+  && ./bin/config set mysql.pass ${MYSQL_PASS}
 set -x
+test -n "${1}" \
+  && ARG=$(echo ${1:-start}  | tr [A-Z] [a-z])
 
-# You should set the base URI to the URI you will use to access Phabricator,
-# like "http://phabricator.example.com/".
-#
-# Include the protocol (http or https), domain name, and port number if you are
-# using a port other than 80 (http) or 443 (https).
-./bin/config set phabricator.base-uri "${PHABRICATOR_URI}"
+case "$ARG" in
+  "storage")
+      # FIXME: make this happen on first-run only!
+      # See 'bin/storage status' for possible first-run control points.
+      ./bin/storage upgrade --force && exit
+      ;;
+  "start")
+      # Set the local repository and make sure it is readable by the web user.
+      if [ -n "${REPOSITORY_LOCAL_PATH}" ]; then
+        if [ -d "${REPOSITORY_LOCAL_PATH}" ]; then
+          :
+        else
+          mkdir -p "${REPOSITORY_LOCAL_PATH}"
+        fi
 
-# Set recommended runtime configuration values to silence setup warnings.
-./bin/config set storage.mysql-engine.max-size 8388608
-./bin/config set pygments.enabled true
+        ./bin/config set repository.default-local-path "${REPOSITORY_LOCAL_PATH}" \
+        chown -R www-data:www-data "$REPOSITORY_LOCAL_PATH"
 
-# FIXME: make this happen on first-run only!
-# See 'bin/storage status' for possible first-run control points.
-./bin/storage upgrade --force
+      else
+        echo "No REPOSITORY_LOCAL_PATH set"
+        exit
+      fi
 
-# Start phd service so Phabricator daemons run.
-./bin/phd start
+      # You should set the base URI to the URI you will use to access Phabricator,
+      # like "http://phabricator.example.com/".
 
-exec php-fpm
+      # Include the protocol (http or https), domain name, and port number if you are
+      # using a port other than 80 (http) or 443 (https).
+      test -n "${PHABRICATOR_URI}" \
+       && ./bin/config set phabricator.base-uri "${PHABRICATOR_URI}"
+
+      # Set recommended runtime configuration values to silence setup warnings.
+      ./bin/config set storage.mysql-engine.max-size 8388608
+      ./bin/config set pygments.enabled true
+
+      # Start phd and php-fpm running in the foreground
+      ./bin/phd start && /usr/local/sbin/php-fpm -F
+      ;;
+  "data")
+      # Allows the container to be used as a data-volume only container
+      /bin/true && exit
+      ;;
+  "shell"|"admin")
+      /bin/sh
+      ;;
+  "dump")
+      ./bin/storage dump
+      exit
+      ;;
+  *)
+      exec $ARG
+      ;;
+esac
