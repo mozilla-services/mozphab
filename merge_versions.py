692907ab1cec7cbe5270f4e29c89c5148e497b2d
Chris Kolosiwsky <ckolos@mozilla.com>
2017-04-28 14:19:43 -0500

    minor changes for cloudops
     - entrypoint uses a case statement to allow more functionality in
    container
     - Dockerfile is closer to dockerflow spec wrt users, app location, etc.
     - merge_versions.py to create a single version.json
     - changes in circle.yml to support version.json changes

---
+++ merge_versions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+import json
+import os
+import sys
+
+try:
+    with open('/app/circle.json', 'r') as f:
+        circle_data = json.load(f)
+except IOError:
+    circle_data = {}
+
+app_data = dict()
+app_data = {
+    'arcanist_source': 'https://github.com/phacility/arcanist',
+    'arcanist_version': os.getenv('ARCANIST_GIT_SHA', None),
+    'libphutil_source': 'https://github.com/phacility/libphutil',
+    'libphutil_version': os.getenv('LIBPHUTIL_GIT_SHA', None),
+    'phabricator_source': 'https://github.com/phacility/phabricator',
+    'phabricator_version': os.getenv('PHABRICATOR_GIT_SHA', None),
+}
+app_data.update(circle_data)
+try:
+    with open('/app/version.json', 'w') as f:
+        json.dump(app_data, f)
+except IOError:
+    sys.exit()
