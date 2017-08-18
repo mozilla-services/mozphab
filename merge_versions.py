fb5963492c6d8aab2c81b24c658b0b3602f21422
Chris Kolosiwsky <ckolos@mozilla.com>
2017-08-18 10:30:42 -0500

    use version.json for docker container, not mozphab.json

--- merge_versions.py
+++ merge_versions.py
@@ -4,8 +4,9 @@ import os
 import sys
 
 try:
-    with open('/app/mozphab.json', 'r') as f:
+    with open('/app/version.json', 'r') as f:
         circle_data = json.load(f)
+        f.close()
 except IOError:
     circle_data = {}
 
@@ -20,7 +21,7 @@ app_data = {
 }
 app_data.update(circle_data)
 try:
-    with open('/app/mozphab.json', 'w') as f:
+    with open('/app/version.json', 'w') as f:
         json.dump(app_data, f)
 except IOError:
     sys.exit()
